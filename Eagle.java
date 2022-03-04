f99f06624fb3e212526e7fde57ae0dd4ec0174f7
DanielaReinhardt <reinhardtd@telekom.de>
2022-03-04 10:17:55 +0100

    first try

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,6 @@
-public class Eagle extends Bird {
+
+
+public class Eagle extends Bird implements fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +23,43 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+ 
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s flys upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flys downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it cant land.%n", this.getName());
+        }
+    }
+
+
 }
---
+++ fly.java
@@ -0,0 +1,15 @@
+public interface fly {
+    
+    void takeOff();
+
+    int ascend(int meters);
+
+    int descend(int meters);
+
+    void land();
+    
+    default void glide(){
+        System.out.println("glides into the air.");
+    }
+
+}
